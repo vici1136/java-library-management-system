ddbc30b5342ef58e243491e72bfc7c322254d45c
Dragoman Vicențiu <vdragoman11@gmail.com>
2025-11-11 17:40:49 +0200

    commit cache

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import database.DatabaseConnectionFactory;
 import model.Book;
 import model.builder.BookBuilder;
-import repository.BookRepository;
-import repository.BookRepositoryMock;
-import repository.BookRepositoryMySQL;
+import repository.book.BookRepository;
+import repository.book.BookRepositoryCacheDecorator;
+import repository.book.BookRepositoryMySQL;
+import repository.book.Cache;
 import service.BookService;
 import service.BookServiceImplementation;
 
@@ -31,27 +32,35 @@ public class Main {
 //        bookRepository.removeAll();
 //        System.out.println(bookRepository.findAll());
 
-        Connection connection = DatabaseConnectionFactory.getConnectionWrapper(false).getConnection();
-        BookRepository bookRepository = new BookRepositoryMySQL(connection);
+//        Connection connection = DatabaseConnectionFactory.getConnectionWrapper(false).getConnection();
+//        BookRepository bookRepository = new BookRepositoryMySQL(connection);
+//
+//        BookService bookService = new BookServiceImplementation(bookRepository);
+//
+//        bookService.save(book);
+//
+//        System.out.println(bookService.findAll());
+//
+//        Book bookMoaraCuNoroc = new BookBuilder().setTitle("Moara cu noroc").setAuthor("Ioan Slavici").setPublishDate(LocalDate.of(1950, 2, 10)).build();
+//
+//        bookService.save(bookMoaraCuNoroc);
+//
+//        System.out.println(bookService.findAll());
+//
+//        bookService.delete(bookMoaraCuNoroc);
+//        bookService.delete(book);
+//
+//        System.out.println(bookService.findAll());
+//
+//        bookService.save(book);
+//        System.out.println(bookService.findAll());
 
+        Connection connection = DatabaseConnectionFactory.getConnectionWrapper(false).getConnection();
+        BookRepository bookRepository = new BookRepositoryCacheDecorator(new BookRepositoryMySQL(connection), new Cache<>());
         BookService bookService = new BookServiceImplementation(bookRepository);
 
-        bookService.save(book);
-
+        bookRepository.save(book);
         System.out.println(bookService.findAll());
-
-        Book bookMoaraCuNoroc = new BookBuilder().setTitle("Moara cu noroc").setAuthor("Ioan Slavici").setPublishDate(LocalDate.of(1950, 2, 10)).build();
-
-        bookService.save(bookMoaraCuNoroc);
-
-        System.out.println(bookService.findAll());
-
-        bookService.delete(bookMoaraCuNoroc);
-        bookService.delete(book);
-
-        System.out.println(bookService.findAll());
-
-        bookService.save(book);
         System.out.println(bookService.findAll());
     }
 }
---
+++ src/main/java/repository/book/BookRepositoryDecorator.java
@@ -0,0 +1,9 @@
+package repository.book;
+
+public abstract class BookRepositoryDecorator implements BookRepository {
+    protected BookRepository decoratedBookRepository;
+
+    public BookRepositoryDecorator(BookRepository bookRepository) {
+        decoratedBookRepository = bookRepository;
+    }
+}
